386444b7d14f86c46a63b6f15574a2841816589c
Tatyana <Tatyanaantonova1212@gmail.com>
2022-07-13 17:31:01 +0300

    add html

--- demo/src/main/java/com/example/demo/web/UserRegistrationController.java
+++ demo/src/main/java/com/example/demo/web/UserRegistrationController.java
@@ -3,7 +3,10 @@ package com.example.demo.web;
 import com.example.demo.service.UserService;
 import com.example.demo.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +18,21 @@ public class UserRegistrationController {
         this.userService = userService;
     }
 
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto(){
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(Model model){
+        model.addAttribute("user", new UserRegistrationDto());
+        return "registration";
+    }
+    @GetMapping
+    public String showRegistrationForm(){
+        return "registration";
+    }
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user")UserRegistrationDto registrationDto) {
         userService.save(registrationDto);
         return "redirect:/registration?success";
